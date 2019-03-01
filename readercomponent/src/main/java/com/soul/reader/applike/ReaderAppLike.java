762ca4a016dad933b1a0bb4431319b48e57bba82
walid <walid@soulapp.cn>
2019-03-01 22:21:02 +0800

    push code

--- readercomponent/src/main/java/com/soul/reader/applike/ReaderAppLike.java
+++ readercomponent/src/main/java/com/soul/reader/applike/ReaderAppLike.java
@@ -3,8 +3,6 @@ package com.soul.reader.applike;
 import com.soul.component.componentlib.applicationlike.IApplicationLike;
 import com.soul.component.componentlib.router.Router;
 import com.soul.component.componentlib.router.ui.UIRouter;
-import com.soul.componentservice.readerbook.ReadBookService;
-import com.soul.reader.serviceimpl.ReadBookServiceImpl;
 
 /**
  * Created by mrzhang on 2017/6/15.
@@ -18,13 +16,13 @@ public class ReaderAppLike implements IApplicationLike {
     @Override
     public void onCreate() {
         uiRouter.registerUI("reader");
-        router.addService(ReadBookService.class.getSimpleName(), new ReadBookServiceImpl());
+//        router.addService(ReadBookService.class.getSimpleName(), new ReadBookServiceImpl());
 //        router.addService(ReadBookService.class.getSimpleName(), new ReadBookServiceImplKotlin());
     }
 
     @Override
     public void onStop() {
         uiRouter.unregisterUI("reader");
-        router.removeService(ReadBookService.class.getSimpleName());
+//        router.removeService(ReadBookService.class.getSimpleName());
     }
 }
