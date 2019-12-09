86f19d73dd0f347cb7d633bb1f2ec4151369b9e6
panxinghai <panxinghai@soulapp.cn>
2019-12-09 21:14:58 +0800

    upload gradle

--- app/src/main/java/com/soul/componentdemo/MainActivity.java
+++ app/src/main/java/com/soul/componentdemo/MainActivity.java
@@ -2,11 +2,12 @@ package com.soul.componentdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
